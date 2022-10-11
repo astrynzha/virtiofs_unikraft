385846d3702e1083ca4ac63c1b5e395286cfafbc
Andrii Strynzha <a.strynzha@gmail.com>
2022-10-11 00:01:29 -0700

    Backup

--- lib/benchmarks/include/uk/helper_functions.h
+++ lib/benchmarks/include/uk/helper_functions.h
@@ -17,20 +17,23 @@
 // how many digits a number has. Cannot be less than 1.
 #define DIGITS(num) ((int)((log10_custom(MAX(num, 1u))+1)*sizeof(char)))
 
-#define KB(n) ((BYTES) (n)*1024)
-#define MB(n) ((BYTES) ((n) * KB(1024)))
-#define GB(n) ((BYTES) ((n) * MB(1024)))
-#define B_TO_KB(n) ((BYTES) ((n) / 1024))
-#define B_TO_MB(n) ((BYTES) ((n) / 1024 / 1024))
-#define B_TO_GB(n) ((BYTES) ((n) / 1024 / 1024 / 1024))
-
-typedef unsigned long long BYTES;
 typedef unsigned long FILES;
+typedef unsigned long long BYTES;
 
-BYTES get_file_size(FILE *file);
-BYTES sample_in_range(BYTES lower, BYTES upper);
-void read_bytes(FILE *file, BYTES bytes, BYTES buffer_size);
-void write_bytes(FILE *file, BYTES bytes, BYTES buffer_size);
 void init_filenames(FILES amount, int max_filename_length, char *file_names);
 
-#endif
+
+// #define KB(n) ((BYTES) (n)*1024)
+// #define MB(n) ((BYTES) ((n) * KB(1024)))
+// #define GB(n) ((BYTES) ((n) * MB(1024)))
+// #define B_TO_KB(n) ((BYTES) ((n) / 1024))
+// #define B_TO_MB(n) ((BYTES) ((n) / 1024 / 1024))
+// #define B_TO_GB(n) ((BYTES) ((n) / 1024 / 1024 / 1024))
+
+
+// BYTES get_file_size(FILE *file);
+// BYTES sample_in_range(BYTES lower, BYTES upper);
+// void read_bytes(FILE *file, BYTES bytes, BYTES buffer_size);
+// void write_bytes(FILE *file, BYTES bytes, BYTES buffer_size);
+
+#endif /* HELPER_FUNCTIONS_H */
--- lib/benchmarks/include/uk/measurement_scenarios.h
+++ lib/benchmarks/include/uk/measurement_scenarios.h
@@ -10,13 +10,13 @@
 #include "uk/fusedev_core.h"
 
 __nanosec create_files(struct uk_fuse_dev *fusedev, FILES amount);
-__nanosec remove_files(FILES amount);
-__nanosec list_dir(FILES file_amount);
+// __nanosec remove_files(FILES amount);
+// __nanosec list_dir(FILES file_amount);
 
-__nanosec write_seq(BYTES bytes, BYTES buffer_size);
-__nanosec write_randomly(FILE *file, BYTES bytes, BYTES buffer_size, BYTES lower_write_limit, BYTES upper_write_limit);
+// __nanosec write_seq(BYTES bytes, BYTES buffer_size);
+// __nanosec write_randomly(FILE *file, BYTES bytes, BYTES buffer_size, BYTES lower_write_limit, BYTES upper_write_limit);
 
-__nanosec read_seq(FILE *file, BYTES bytes, BYTES buffer_size);
-__nanosec read_randomly(FILE *file, BYTES bytes, BYTES buffer_size, BYTES lower_read_limit, BYTES upper_read_limit);
+// __nanosec read_seq(FILE *file, BYTES bytes, BYTES buffer_size);
+// __nanosec read_randomly(FILE *file, BYTES bytes, BYTES buffer_size, BYTES lower_read_limit, BYTES upper_read_limit);
 
 #endif
